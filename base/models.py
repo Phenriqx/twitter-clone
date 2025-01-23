3e80c80113b171ba34655ba8a7bf1ed82f91e93f
pedrohenrique <pedroxlbd@gmail.com>
2025-01-23 04:38:13 -0300

    basic posting system

--- base/models.py
+++ base/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    
+    name = models.CharField(max_length=50, null=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(max_length=200, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    #media = models.ImageField()
+
+    def __str__(self):
+        return self.name
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    
+class Post(models.Model):
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=300)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    #media 
+    
+    class Meta:
+        ordering = ['-updated', '-created']
+        
+    def __str__(self):
+        return f'{self.author.id} - {self.author}: {self.content[:50] if len(self.content) > 50 else self.content}'
--- base/views.py
+++ base/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect
+from .models import User, Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'home.html', context)
+
+def loadPosts(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {'post': post}
+    return render(request, 'posts.html', context)
