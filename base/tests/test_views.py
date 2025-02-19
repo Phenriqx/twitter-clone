03bffc69367621fc26e1ef69f70000d120befe6f
Pedro Henrique <pedroxlbd@gmail.com>
2025-02-19 20:25:46 -0300

    test(auth) added testing for user authentication

--- base/tests/test_views.py
+++ base/tests/test_views.py
@@ -7,3 +7,20 @@ class TestHomePage(SimpleTestCase):
     def test_home_page_status_code(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 302)
+        
+
+class TestProfilePage(TestCase):
+    
+    def test_profile_page_redirects_anonymous_users(self):
+        response = self.client.get(reverse('link-profile', kwargs={'username': 'testuser'}))
+        self.assertRedirects(response,
+                             expected_url=f"{reverse('login')}?next={reverse('link-profile', kwargs={'username': 'testuser'})}"
+        )
+    
+    def test_profile_page_accessible_for_authenticated_users(self):
+        User.objects.create(username='testuser', name='test', email='test@gmail.com')
+        
+        self.client.login(username='testuser', name='test', email='test@gmail.com')
+        response = self.client.get(reverse('link-profile', kwargs={'username': 'testuser'}))
+        
+        self.assertEqual(response.status_code, 302)
